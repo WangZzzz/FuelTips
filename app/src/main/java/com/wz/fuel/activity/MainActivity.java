6700a32e353524fc98f1a8199e0a0f7e78bc3d12
wangzhe15 <wangzhe15@cmbc.com.cn>
2017-06-05 19:05:01 +0800

    增加油价功能

--- app/src/main/java/com/wz/fuel/activity/MainActivity.java
+++ app/src/main/java/com/wz/fuel/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wz.fuel.activity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.viewpagerindicator.TabPageIndicator;
 import com.wz.activity.WBaseActivity;
 import com.wz.fuel.AppConstants;
@@ -20,6 +22,7 @@ import com.wz.fuel.adapter.MainFragmentAdapter;
 import com.wz.fuel.fragment.FuelPriceFragment;
 import com.wz.fuel.fragment.FuelRecordFragment;
 import com.wz.util.ToastMsgUtil;
+import com.wz.util.WLog;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,11 +45,16 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
     @BindView(R.id.tabIndicator)
     TabPageIndicator mTabIndicator;
 
+    private ProgressDialog mDialog;
+
     private static final String[] TAB_TITLES = {"今日油价", "加油统计"};
     private static final int[] TAB_ICON_RES_IDS = {R.drawable.ic_fuel_price, R.drawable.ic_add_fuel};
 
     private MainFragmentAdapter mAdapter;
     private List<Fragment> mFragments;
+
+    private FuelRecordFragment mFuelRecordFragment;
+    private FuelPriceFragment mFuelPriceFragment;
 
     private LocationClient mLocationClient;
 
@@ -61,12 +69,15 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
     private void init() {
         mLocationClient = new LocationClient(this);
         mLocationClient.registerLocationListener(mLocationListener);
+        LocationClientOption option = mLocationClient.getLocOption();
+        //需要设置此项，否则无法获取具体的地址
+        option.setIsNeedAddress(true);
         initToolbar();
         initTabHost();
     }
 
     private void initToolbar() {
-        mToolbarTitle.setText("加油助手");
+        mToolbarTitle.setText("今日油价");
         if (TextUtils.isEmpty(AppConstants.sProvince)) {
             mTvProvince.setText("定位中");
             mLocationClient.start();
@@ -78,11 +89,29 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
 
     private void initTabHost() {
         mFragments = new ArrayList<>();
-        mFragments.add(new FuelPriceFragment());
-        mFragments.add(new FuelRecordFragment());
+        mFuelPriceFragment = new FuelPriceFragment();
+        mFuelRecordFragment = new FuelRecordFragment();
+        mFragments.add(mFuelPriceFragment);
+        mFragments.add(mFuelRecordFragment);
         mAdapter = new MainFragmentAdapter(getSupportFragmentManager(), mFragments, TAB_TITLES, TAB_ICON_RES_IDS);
         mViewPager.setAdapter(mAdapter);
         mTabIndicator.setViewPager(mViewPager);
+        mTabIndicator.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                mToolbarTitle.setText(TAB_TITLES[position]);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
@@ -110,6 +139,7 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
                     }
                 } else if (resultCode == RESULT_OK) {
                     mTvProvince.setText(AppConstants.sProvince);
+                    mFuelPriceFragment.refresh();
                 }
                 break;
         }
@@ -119,7 +149,10 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
         @Override
         public void onReceiveLocation(BDLocation bdLocation) {
             if (bdLocation != null && bdLocation.getProvince() != null) {
-                AppConstants.sProvince = bdLocation.getProvince();
+                AppConstants.sProvince = processProvinceStr(bdLocation.getProvince());
+                //重新获取当前价格
+                mFuelPriceFragment.refresh();
+                WLog.d(TAG, "addr : " + bdLocation.getAddrStr());
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -131,6 +164,7 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
                 Toasty.info(MainActivity.this, "定位失败，请手动选择省份！", 1).show();
                 chooseProvince();
             }
+            mLocationClient.stop();
         }
 
         @Override
@@ -138,4 +172,29 @@ public class MainActivity extends WBaseActivity implements View.OnClickListener 
 
         }
     };
+
+    private String processProvinceStr(String provnice) {
+        if (!TextUtils.isEmpty(provnice)) {
+            if (provnice.contains("黑龙江") || provnice.contains("内蒙古")) {
+                return provnice.substring(0, 3);
+            } else {
+                return provnice.substring(0, 2);
+            }
+        }
+        return provnice;
+    }
+
+    private void showProgressDialog(String msg) {
+        if (mDialog == null) {
+            mDialog = new ProgressDialog(this);
+            mDialog.setTitle("提示");
+            mDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mDialog.setCancelable(false);
+            mDialog.setCanceledOnTouchOutside(false);
+        }
+    }
+
+    private void hideProgressDialog() {
+
+    }
 }
--- app/src/main/java/com/wz/fuel/fragment/FuelPriceFragment.java
+++ app/src/main/java/com/wz/fuel/fragment/FuelPriceFragment.java
@@ -4,9 +4,11 @@ package com.wz.fuel.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.wz.fragment.WBaseFragment;
 import com.wz.fuel.AppConstants;
@@ -15,14 +17,31 @@ import com.wz.fuel.mvp.bean.FuelBean;
 import com.wz.fuel.mvp.presenter.FuelPricePresenter;
 import com.wz.fuel.mvp.view.IView;
 import com.wz.util.ToastMsgUtil;
+import com.wz.util.WLog;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FuelPriceFragment extends WBaseFragment implements IView<FuelBean> {
+    private static final String TAG = FuelPriceFragment.class.getSimpleName();
+    @BindView(R.id.tv_price_gas_89)
+    TextView mTvPriceGas89;
+    @BindView(R.id.tv_price_gas_92)
+    TextView mTvPriceGas92;
+    @BindView(R.id.tv_price_gas_95)
+    TextView mTvPriceGas95;
+    @BindView(R.id.tv_price_diesel_0)
+    TextView mTvPriceDiesel0;
+    Unbinder unbinder;
+
+    private FuelPricePresenter mPresenter;
 
     public FuelPriceFragment() {
         // Required empty public constructor
@@ -32,17 +51,34 @@ public class FuelPriceFragment extends WBaseFragment implements IView<FuelBean> 
     @Override
     public View initView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fuel_price, container, false);
+        unbinder = ButterKnife.bind(this, view);
         initData();
         return view;
     }
 
-    public void refresh() {
+    @Override
+    public void onStart() {
+        super.onStart();
+        WLog.d(TAG, "onStart");
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        WLog.d(TAG, "onResume");
+    }
+
+    public void refresh() {
+        if (!TextUtils.isEmpty(AppConstants.sProvince)) {
+            mPresenter.queryPrice();
+        }
     }
 
     private void initData() {
-        FuelPricePresenter fuelPricePresenter = new FuelPricePresenter(this);
-        fuelPricePresenter.queryPrice();
+        mPresenter = new FuelPricePresenter(this);
+        if (!TextUtils.isEmpty(AppConstants.sProvince)) {
+            mPresenter.queryPrice();
+        }
     }
 
     @Override
@@ -52,12 +88,23 @@ public class FuelPriceFragment extends WBaseFragment implements IView<FuelBean> 
 
     @Override
     public void onSuccess(List<FuelBean> fuelBeenList) {
-        if (fuelBeenList != null && fuelBeenList.size() > 0) {
+        if (!TextUtils.isEmpty(AppConstants.sProvince) && fuelBeenList != null && fuelBeenList.size() > 0) {
             for (FuelBean fuelBean : fuelBeenList) {
                 if (AppConstants.sProvince.equals(fuelBean.province)) {
-                    ToastMsgUtil.info(getActivity(), fuelBean.toString(), 1);
+                    setPrice(fuelBean);
                 }
             }
+        } else {
+            ToastMsgUtil.info(getActivity(), "请先选择省份信息！", 1);
+        }
+    }
+
+    private void setPrice(FuelBean fuelBean) {
+        if (fuelBean != null) {
+            mTvPriceDiesel0.setText(fuelBean.price_diesel_0 + "");
+            mTvPriceGas89.setText(fuelBean.price_gas_89 + "");
+            mTvPriceGas92.setText(fuelBean.price_gas_92 + "");
+            mTvPriceGas95.setText(fuelBean.price_gas_95 + "");
         }
     }
 
@@ -69,5 +116,11 @@ public class FuelPriceFragment extends WBaseFragment implements IView<FuelBean> 
     @Override
     public void hideProgressDialog() {
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
