27ffd58aa405c232d8c86de87b0054e5debc564b
wangzhe15 <wangzhe15@cmbc.com.cn>
2017-08-03 19:09:32 +0800

    1

--- app/src/main/java/com/wz/fuel/activity/BaseActivity.java
+++ app/src/main/java/com/wz/fuel/activity/BaseActivity.java
@@ -1,9 +1,19 @@
 package com.wz.fuel.activity;
 
+import android.graphics.Color;
+import android.os.Bundle;
+
 import com.wz.activity.WBaseActivity;
 import com.wz.fuel.R;
+import com.wz.util.StatusBarUtil;
 
 public class BaseActivity extends WBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StatusBarUtil.setColorBar(this, Color.parseColor("#3A94FF"));
+    }
 
     /**
      * 使用动画效果结束
