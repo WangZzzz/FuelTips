8d10ba88ac1e8a36a4d521f48a2084c5aad84c0c
WangZzzz <wztc_001@bupt.edu.cn>
2019-06-05 10:34:43 +0800

    删除注释

--- common_lib/src/main/java/com/wz/util/WifiUtil.java
+++ common_lib/src/main/java/com/wz/util/WifiUtil.java
@@ -4,20 +4,6 @@ import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
-/**
- * <br>
- * FIREFLY
- * <p>
- * com.wz.util
- *
- * @author wangzhe
- * @version 3.2.0
- * @date 2017/5/17 14:57
- * @api 7
- * <br>
- * CMBC-版权所有
- * <br>
- */
 public class WifiUtil {
     public static void startAp(Context context) {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
