161583936962871589f089543c1713271fb1c790
WangZzzz <wztc_001@bupt.edu.cn>
2019-06-05 10:33:27 +0800

    删除注释

--- common_lib/src/main/java/com/wz/util/NumberUtil.java
+++ common_lib/src/main/java/com/wz/util/NumberUtil.java
@@ -2,20 +2,6 @@ package com.wz.util;
 
 import java.text.DecimalFormat;
 
-/**
- * <br>
- * FIREFLY
- * <p>
- * com.wz.util
- *
- * @author wangzhe
- * @version 3.2.0
- * @date 2017/6/27 10:54
- * @api 7
- * <br>
- * CMBC-版权所有
- * <br>
- */
 public class NumberUtil {
     public static boolean isZero(float f) {
         return Math.abs(f) < 1e-6;
