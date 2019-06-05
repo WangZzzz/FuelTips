0debc423ca49db7c421ea069a9538e862021a8f8
WangZzzz <wztc_001@bupt.edu.cn>
2019-06-05 10:35:43 +0800

    删除注释

--- app/src/main/java/com/wz/fuel/mvp/view/IFuelRecordView.java
+++ app/src/main/java/com/wz/fuel/mvp/view/IFuelRecordView.java
@@ -4,20 +4,6 @@ import com.wz.fuel.mvp.bean.FuelRecordBean;
 
 import java.util.List;
 
-/**
- * <br>
- * FIREFLY
- * <p>
- * com.wz.fuel.mvp.view
- *
- * @author wangzhe
- * @version 3.2.0
- * @date 2017/6/6 10:21
- * @api 7
- * <br>
- * CMBC-版权所有
- * <br>
- */
 public interface IFuelRecordView extends IView<FuelRecordBean> {
     void onLoadFromDb(List<FuelRecordBean> fuelRecordList);
 
